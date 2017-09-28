392302e601af8cbeb49e5d4426303f765c62bf98
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-28 17:18:58 +0200

    some more progress, added user input to file

--- src/TodoApp.java
+++ src/TodoApp.java
@@ -1,27 +1,38 @@
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class TodoApp {
     public static void main(String[] args) {
+        ArrayList<Task> tasks = new ArrayList<>();
+        Path taskPath = Paths.get( "./Assets/tasks.txt" );
+        Task myNewTask = null;
 
         if (args.length != 0) {
             if (args[0].equals( "-l" )) {
                 Usage.writeLines( "./Assets/tasks.txt" );
             } else if (args[0].equals( "-a" )) {
+                try {
+                    List<String> valami = new ArrayList<>();
+                    valami.add( args[1] );
+                    Files.write( taskPath, valami, StandardOpenOption.APPEND );
+                    System.out.println( "Added to the list." );
+                } catch (IOException e) {
+                    System.out.println( "Unable to write file: " + taskPath );
+                }
                 System.out.println( "Add task" );
-//                Scanner sc = new Scanner( System.in );
-//                args[1] = sc.next();
-//                Files.write(Paths.get( "./Assets/tasks.txt" , args[1]));
             } else if (args[0].equals( "-r" )) {
-                System.console().writer().println( "Remove task" );
+                System.out.println( "Remove task" );
             } else if (args[0].equals( "-c" )) {
-                System.console().writer().println( "Complete task" );
+                System.out.println( "Complete task" );
             } else {
                 System.out.println( "Unsupported argument\n" );
             }
+        } else {
+            Usage.writeLines( "./Assets/printusage.txt" );
         }
-        Usage.writeLines( "./Assets/printusage.txt" );
-
     }
 }
