481f4a7f54438918c2cd726a73f65df4862b28c1
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-28 16:12:32 +0200

    Todo, argument error handled with error message

--- src/TodoApp.java
+++ src/TodoApp.java
@@ -1,19 +1,27 @@
+import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class TodoApp {
     public static void main(String[] args) {
 
-        if (args[0].equals( "-l" )) {
-            Usage.writeLines( "/home/dzsofa/greenfox/dzsofa-todo-app/Assets/tasks.txt" );
-        } else if (args[0].equals( "-a" )) {
-            System.console().writer().println("Add task");
-        } else if (args[0].equals( "-r" )) {
-            System.console().writer().println("Remove task");
-        } else if (args[0].equals( "-c" )) {
-            System.console().writer().println("Complete task");
+        if (args.length != 0) {
+            if (args[0].equals( "-l" )) {
+                Usage.writeLines( "./Assets/tasks.txt" );
+            } else if (args[0].equals( "-a" )) {
+                System.out.println( "Add task" );
+//                Scanner sc = new Scanner( System.in );
+//                args[1] = sc.next();
+//                Files.write(Paths.get( "./Assets/tasks.txt" , args[1]));
+            } else if (args[0].equals( "-r" )) {
+                System.console().writer().println( "Remove task" );
+            } else if (args[0].equals( "-c" )) {
+                System.console().writer().println( "Complete task" );
+            } else {
+                System.out.println( "Unsupported argument\n" );
+            }
         }
-        else {
-            Usage.writeLines( "/home/dzsofa/greenfox/dzsofa-todo-app/Assets/printusage.txt" );
-        }
+        Usage.writeLines( "./Assets/printusage.txt" );
+
     }
 }
