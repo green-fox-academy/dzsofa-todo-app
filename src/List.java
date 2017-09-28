2a9efe7e79b4bea871025eb3b27e0f35331baffa
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-28 12:11:45 +0200

    TodoApp, List class created for tasks

---
+++ src/List.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class List {
+    private ArrayList<Task> tasks;
+
+    public List() {
+        tasks = new ArrayList<>();
+    }
+
+    public void addTask(Task task) {
+        tasks.add( task );
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        for (int i = 0; i < tasks.size(); i++) {
+            result += (i + 1) + ". " + tasks.get( i ) + "\n";
+        }
+        return result;
+    }
+}
