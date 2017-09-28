3be3dc849c654959353ebdc6870e148a7e4b6d9d
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-28 13:53:45 +0200

    TodoApp, conditions for commands added, now with default text

--- src/TodoApp.java
+++ src/TodoApp.java
@@ -3,6 +3,17 @@ import java.nio.file.Paths;
 public class TodoApp {
     public static void main(String[] args) {
 
-        Usage.writeLines( Paths.get( "/home/dzsofa/greenfox/dzsofa-todo-app/Assets/printusage.txt" ) );
+        if (args[0].equals( "-l" )) {
+            Usage.writeLines( "/home/dzsofa/greenfox/dzsofa-todo-app/Assets/tasks.txt" );
+        } else if (args[0].equals( "-a" )) {
+            System.console().writer().println("Add task");
+        } else if (args[0].equals( "-r" )) {
+            System.console().writer().println("Remove task");
+        } else if (args[0].equals( "-c" )) {
+            System.console().writer().println("Complete task");
+        }
+        else {
+            Usage.writeLines( "/home/dzsofa/greenfox/dzsofa-todo-app/Assets/printusage.txt" );
+        }
     }
 }
