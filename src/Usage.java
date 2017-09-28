a50dd3e4b539e28747c47d546de01adcda458d19
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-28 11:17:36 +0200

    todo app, user manual can be printed in Terminal

--- src/Usage.java
+++ src/Usage.java
@@ -1,22 +1,21 @@
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 
 public class Usage {
     public static void main(String[] args) {
-        Path usagePath = Paths.get("./printusage.txt");
+        Path usagePath = Paths.get( "/home/dzsofa/greenfox/dzsofa-todo-app/Assets/printusage.txt" );
         try {
-            System.console().writer().println(Files.readAllLines(usagePath));
-            System.console().writer().println();
+            List<String> allLines = Files.readAllLines( usagePath );
+            for (String lines : allLines) {
+                System.console().writer().print(lines);
+                System.console().writer().println();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-//            List<String> userMan = Files.readAllLines(usagePath);
-//            System.console().writer().println(userMan);
     }
 }
 
